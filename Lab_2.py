ddc0b0fa62348d687fa14486c66f9dcd7eaf42e9
Oniskiv02 <Oniskiv02@gmail.com>
2024-11-05 14:47:42 +0200

    New Words

--- Lab_2.py
+++ Lab_2.py
@@ -4,6 +4,8 @@ while True :
         print("Добрий день")
     elif msg=="Слава Україні" :
         print("Героям слава")
+    elif msg=="Hello" :
+        print("Hi")
     elif msg=="pa pa" :
         print("the end")
         break
