784dd443f44bd40fc561dd367506d6367c16c2d0
Oniskiv02 <Oniskiv02@gmail.com>
2024-11-05 14:15:34 +0200

    New code

---
+++ Lab_2.py
@@ -0,0 +1,11 @@
+while True :
+    msg=input("please input a msg ")
+    if msg=="Добрий день" :
+        print("Добрий день")
+    elif msg=="Слава Україні" :
+        print("Героям слава")
+    elif msg=="pa pa" :
+        print("the end")
+        break
+    else : 
+        print ("Спробуй ще")
